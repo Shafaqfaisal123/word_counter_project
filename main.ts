d1d4ed93be578514fe0310267e356479da4e434e
Shafaqfaisal123 <mrsfaisalfudda@gmail.com>
2024-05-07 15:11:40 -0700

    Adding a word counter project

---
+++ main.ts
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+console.log(chalk.bold.cyanBright("\n \t\t Code with MrsFaisal - word counter"));
+console.log(("=".repeat(60)));
+
+let answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "enter a sentence",
+    }
+]);
+
+let words = answers.sentence.trim().split(" ");
+
+console.log("=".repeat(60));
+console.log(chalk.bold("sentence words:"));
+console.log(words);
+console.log(chalk.bold(`\n - word count: ${chalk.bold.redBright(words.length)}`));
+console.log("=".repeat(60));
+
